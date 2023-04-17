2ddfd1fd94b62c94e3f4d5c62763fe72f651206c
= <=>
2023-04-17 18:54:19 +0400

    Initial commit from toptech

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const roboto = Inter({ subsets: ['latin'], weight: '400' })
+
+export default function Home() {
+  return (
+    <main>Test</main>
+  )
+}
